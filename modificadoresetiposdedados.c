4af3ffbbb3b762f9d58bcf071418b7bd4dbbb6f1
DRAKEOVER <119982378+DRAKEOVER@users.noreply.github.com>
2025-03-27 00:50:06 +0000

    desafio novato tema2

--- modificadoresetiposdedados.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- int main (){
-   int king= 3000000000;
-    printf("king; %d\n", king);
-    king= (long) 3000000000;
-    printf("king; %ld\n", king);
-   king=(long long) 3000000000;
-    printf("king; %lld\n", king);
- }
